79e14e6dbe22a12915243f576be8e4f865edbd3c
Aditya Ranjan Jha <adityaranjan2014@gmail.com>
2024-03-24 05:24:26 +0530

    Adding auth using clerk

--- discord-pro/app/(main)/(routes)/page.tsx
+++ discord-pro/app/(main)/(routes)/page.tsx
@@ -1,11 +1,14 @@
 
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import test from "node:test";
 
 export default function Home() {
   return (
     <div>
-      This is a protected route
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
   );
 }
--- discord-pro/app/layout.tsx
+++ discord-pro/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ThemeProvider>
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
+    </ClerkProvider>
   );
 }
